491a4022c301a938e34b577a334e6575eb3a1449
王磊 <sysuleiw@163.com>
2014-07-10 16:56:42 +0800

    过滤掉a链接,提高准确率

---
+++ common.py
@@ -0,0 +1,70 @@
+# coding:utf8
+import codecs
+import traceback
+import multiprocessing
+import re
+
+
+class Config(object):
+
+    '''存放相关配置项
+    '''
+    # 白名单
+    white_list = '/home/apache/htdocs/wml/foxyserver/analy/data/maxcms_mode_sites.txt'
+
+    # 网页文件目录
+    pages = '/home/apache/htdocs/wml/foxyserver/analy/page/'
+    #pages = './test/'
+
+    # 存放通过分类关键字识别出来的类别域名列表
+    classify = './keywords_classify/'
+
+    # 存放通过计算编辑距离识别出来的类别域名列表
+    edit_dist = './distance/'
+
+    # 编辑距离相似度
+    edit_dist_benchmark = 0.9
+
+    ipc_list_url = multiprocessing.Manager().list()
+    ipc_r_kw = multiprocessing.Manager().Value('', '')
+
+    filter_head = re.compile(r'(<head>[\s\S]*</head>)|\s+')  # 过滤head中的内容,降低差异度
+    # 过滤掉body中的a链接,降低差异度
+    filter_anchor = re.compile('<a[\s\S]*?>[\s\S]*?<\/a>')
+
+
+class FileOper(object):
+
+    '''文件操作类
+    文件操作,读取文件,写入文件,根据值写入,根据数组写入等等
+    '''
+
+    def file_read(self, file_path):
+        '''读取文件内容,返回一个字符串
+        '''
+        try:
+            with open(file_path, 'r') as f:
+                return f.read().replace('\n', '').strip()  # 首先过滤字符串前后空格
+        except:
+            pass
+
+    def file_readlines(self, file_path):
+        '''读取文件内容,返回一个数组
+        '''
+        result = []
+        with open(file_path, 'r') as f:
+            for line in f.readlines():
+                result.append(line.replace('\n', '').strip())  # 首先过滤字符串前后空格
+        return result
+
+    def file_writelines(self, file_path, lines):
+        '''写入内容到某个文件中,此时的内容是一个数组
+        '''
+        try:
+            fw = codecs.open(file_path, 'w', 'utf-8')
+            fw.writelines(lines)
+            fw.close()
+        except:
+            print 'error!:'
+            print traceback.format_stack()
+            fw.close()
--- main.py
+++ main.py
@@ -1,77 +1,74 @@
-#coding:utf8
-from config import *
-import re
+# coding:utf8
 import time
 import multiprocessing
 import keywords
-from  Levenshtein import jaro
+from Levenshtein import jaro
+from common import *
 
-def get_distance_contrasted(file):
-  '''获取编辑距离参照物文件
-  '''
-  page_content = []
-  for url in keywords.kv.values():
-    filepath = Config.pages+url+'.html'
-    content = file.file_read(filepath)
-    if content:
-      if content.find('gb2312')> -1 or content.find('gbk')> -1:
-        content = content.decode('gbk','ignore')
-      else:
-        content = content.decode('utf8','ignore')
-      content = Config.filter.sub('',content) #过滤head之间的内容
-      page_content.append((content,url))
-      break
-  return page_content
 
-def get_page_cont(file):
-  '''首先获取所有网页内容,避免重复I/O
-  '''
-  page_content = []
-  wl = file.file_readlines(Config.white_list)
-  for url in wl:
-    filepath = Config.pages+url+'.html'
-    content = file.file_read(filepath)
-    if content:
-      if content.find('gb2312')> -1 or content.find('gbk')> -1:
-        content = content.decode('gbk','ignore')
-      else:
-        content = content.decode('utf8','ignore')
-      content = Config.filter.sub('',content) #过滤head之间的内容
-      page_content.append((content,url))
-  return page_content
+def get_page_cont(file, url_list):
+    ''' 根据url列表获取对应的url文件内容
+    '''
+    page_content = []
+    for url in url_list:
+        filepath = Config.pages + url + '.html'
+        content = file.file_read(filepath)
+        if content:
+            if content.find('gb2312') > -1 or content.find('gbk') > -1:
+                content = content.decode('gbk', 'ignore')
+            else:
+                content = content.decode('utf8', 'ignore')
+            content = Config.filter_head.sub('', content)  # 过滤head之间的内容
+            content = Config.filter_anchor.sub('', content)  # 过滤掉所有的a链接
+            page_content.append((content, url))
+    return page_content
 
-def get_classify(cont):
-  '''判断当前站点所属类别,该函数为进程池目标函数
-  '''
-  try:
-    if jaro(Config.ipc_r_kw.value[0],cont[0]) > 0.9:
-      Config.ipc_list_url.append(cont[1] + '\n')
-  except:
-    print '#############################################'
-    print Config.ipc_r_kw.value[1]
 
-def main_distance():
-  '''通过编辑距离分类入口函数
-  1. 首先取出每一个类别站点内容,过滤掉head部分
-  2. 利用3000个白名单站点几次计算相似度
-  3. 相似度超过80%的放入一个类别
-  '''
-  file = FileOper()
-  page_distance_contrasted = get_distance_contrasted(file)
-  page_content = get_page_cont(file)
-  pool = multiprocessing.Pool(multiprocessing.cpu_count())
-  start = time.time()
-  for idx,cont in enumerate(page_distance_contrasted):
-    while len(Config.ipc_list_url):
-      Config.ipc_list_url.pop()
-    Config.ipc_r_kw.set(cont)
-    pool.map(get_classify,page_content)
-    write_file = Config.edit_dist + cont[1] + '.txt'
-    file.file_writelines(write_file,Config.ipc_list_url)
-    print 'idx:' + str(idx)
+def get_classify(tuple_cont):
+    '''判断当前站点所属类别,该函数为进程池目标函数
+    '''
+    try:
+        if jaro(Config.ipc_r_kw.value[0], tuple_cont[0]) > Config.edit_dist_benchmark:
+            Config.ipc_list_url.append(tuple_cont[1] + '\n')
+    except:
+        print '#############################################'
+        print Config.ipc_r_kw.value[1]
 
-  print time.time()-start
+
+def main():
+    '''通过编辑距离分类入口函数
+    1. 首先取出每一个类别站点内容,过滤掉head部分
+    2. 利用3000个白名单站点几次计算相似度
+    3. 相似度超过80%的放入一个类别
+    '''
+    file = FileOper()
+
+    # 获取编辑距离参照物文件
+    url_list = keywords.kv.values()
+    page_distance_contrasted = get_page_cont(file, url_list)
+
+    # 获取白名单文件,防止重复进行I/O操作
+    url_list = file.file_readlines(Config.white_list)
+    page_content = get_page_cont(file, url_list)
+
+    # 初始化进程池
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+
+    start = time.time()
+
+    for idx, tuple_cont in enumerate(page_distance_contrasted):
+        # 每一个被比较的url被比较前需要清空共享对象
+        while len(Config.ipc_list_url):
+            Config.ipc_list_url.pop()
+
+        Config.ipc_r_kw.set(tuple_cont)
+        pool.map(get_classify, page_content)
+        write_file = Config.edit_dist + tuple_cont[1] + '.txt'
+        file.file_writelines(write_file, Config.ipc_list_url)
+        print 'idx:' + str(idx)
+
+    print time.time() - start
+
 
 if __name__ == "__main__":
-
-  main_distance()
+    main()
